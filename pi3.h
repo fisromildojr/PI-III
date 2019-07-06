df9447637a5f5aafa7bf1bc97bb338147937d6d7
Romildo Jr <fis.romildojr@gmail.com>
2019-07-06 20:42:02 -0300

    Implementação das funções de cadastramento de funcionarios e de lançamento de horas

--- pi3.h
+++ pi3.h
@@ -13,13 +13,29 @@ typedef struct TipoFuncionario{
 	char UF[3];
 } TipoFuncionario;
 
+typedef struct TipoControleHoras{
+	char TipoDia[2];
+	char Data[12];
+	char HoraEntrada1[10];
+	char HoraSaida1[10];
+	char HoraEntrada2[10];
+	char HoraSaida2[10];
+	char Horas100[10];
+	char Horas50[10];
+	char AdcNoturno[10];	
+	char IdFuncionario[10];
+	char IdPeriodo[10];
+} TipoControleHoras;
+
 void ConectaDB(MYSQL conexao);
 
-void Cabecalho(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
+int Cabecalho(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos, int Op);
 
 void MenuPrincipal();
 
 void InserirFuncionario(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
+
+void LancarHoras(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
 
 void CadastrarCartaoPonto();
 
@@ -27,8 +43,12 @@ void GerarEspelho();
 
 void GerarHolerite();
 
-void ExibirFuncionario();
+void ExibirFuncionario(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
+
+void ExibirCargo(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
 
 void ExibirEspelho();
 
 void ExibirHolerite();
+
+void ExibirPeriodo(MYSQL conexao, MYSQL_RES *resp, MYSQL_ROW linhas, MYSQL_FIELD *campos);
